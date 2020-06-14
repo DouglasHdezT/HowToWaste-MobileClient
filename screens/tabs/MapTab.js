329e7bc055ba087551eca4bb5efb2f14111d6f8f
Carlos Antonio Minero Dubon <carlosminerodubon@gmail.com>
2020-06-13 20:16:42 -0600

    google places

--- screens/tabs/MapTab.js
+++ screens/tabs/MapTab.js
@@ -1,12 +1,46 @@
-import React from 'react';
-
+import React,{useEffect,useState} from 'react';
+import * as Location from 'expo-location';
 import { View, Text, StyleSheet,Dimensions } from 'react-native'
 import MapView from "react-native-maps";
+import Search from "../forms/Search";
 
 const MapTab = props => {
+	const [region, setRegion] = useState(null);
+	const [errorMsg, setErrorMsg] = useState(null);
+	console.log(region)
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestPermissionsAsync();
+			if (status !== 'granted') {
+				setErrorMsg('Permission to access location was denied');
+			}
+
+			let location = await Location.getCurrentPositionAsync({});
+			setRegion({
+				latitude: location.coords.latitude,
+				longitude: location.coords.longitude,
+				latitudeDelta:  0.005,
+				longitudeDelta: 0.005,
+			});
+		})();
+	},[]);
+
+	const updateRegion = (lat, lng) => {
+		setRegion({
+			...region,
+			latitude: lat,
+			longitude: lng,
+		});
+	};
 	return (
 		<View style={styles.container}>
-			<MapView style={styles.mapStyle} />
+			<MapView
+				region={region}
+				showsUserLocation
+				loadingEnabled
+				style={styles.mapStyle}
+			/>
+			<Search onSelectLocation={updateRegion}/>
 		</View>
 	);
 }
@@ -19,8 +53,8 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 	},
 	mapStyle: {
-		width: Dimensions.get('window').width,
-		height: Dimensions.get('window').height,
+		width:"100%",
+		height:"100%"
 	},
 });
 
